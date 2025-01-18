f8c93cb93d905d4f4e48f4df20d8805031fd06f7
KaneV <votrungkien05032002@gmail.com>
2025-01-18 11:28:05 +0700

    Balloon Obstacle

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleRaycast();
+            return;
+        }
+    }
+
+    private static void HandleRaycast()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var hits = new List<RaycastHit2D>();
+        hits = Physics2D
+            .RaycastAll(mousePosition, Vector2.zero)
+            .Where(hit => hit.collider.GetComponent<IClickable>() is not null)
+            .ToList();
+        if (hits.Count == 0)
+            return;
+
+        foreach (var clickable in hits.Select(hit => hit.collider.GetComponent<IClickable>()))
+        {
+            clickable.HandleClick();
+        }
+    }
+}
