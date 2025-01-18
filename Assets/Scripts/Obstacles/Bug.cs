c61ec71c3877c62ddecdd3244309e49cf1c7f6c9
KaneV <votrungkien05032002@gmail.com>
2025-01-18 12:08:26 +0700

    add bug obstacle

---
+++ Assets/Scripts/Obstacles/Bug.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Bug : MonoBehaviour
+{
+    [SerializeField]
+    private float speedY = 1f;
+
+    [SerializeField]
+    private float speedX = 1f;
+
+    [SerializeField]
+    private float threshold = 2f;
+
+    private const string BUBBLE_TAG = "Bubble";
+
+    private Rigidbody2D _rigidbody;
+    private Camera _cam;
+
+    private float _currentAngle = 0f;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag(BUBBLE_TAG))
+            return;
+
+        Destroy(other.gameObject);
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _cam = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        _currentAngle += Time.fixedDeltaTime * speedY;
+        var yPos = threshold * Mathf.Sin(_currentAngle);
+
+        _rigidbody.velocity = new Vector2(
+            -1 * speedX * Time.fixedDeltaTime,
+            yPos * Time.fixedDeltaTime
+        );
+
+        CheckOutOfScreen();
+    }
+
+    private void CheckOutOfScreen()
+    {
+        if (transform.position.x < _cam.ScreenToWorldPoint(Vector3.zero).x)
+            Destroy(gameObject);
+    }
+}
