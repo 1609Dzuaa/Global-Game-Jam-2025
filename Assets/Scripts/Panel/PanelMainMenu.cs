f62dd2222d2b9ba1a5ce2cfc00c3e00a42c7b994
Huy <tranlehuy2107@gmail.com>
2025-01-18 11:11:37 +0700

    UI Manager

---
+++ Assets/Scripts/Panel/PanelMainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMainMenu : MonoBehaviour
+{
+    [SerializeField] private Button _btnStart;
+    [SerializeField] private Button _btnOption;
+    [SerializeField] private Button _btnExit;
+
+    private void OnEnable()
+    {
+        _btnStart.onClick.AddListener(OnClickedStart);
+        _btnOption.onClick.AddListener(OnClickedOption);
+        _btnExit.onClick.AddListener(OnClickedExit);
+    }
+
+    private void OnDisable()
+    {
+        _btnStart.onClick.RemoveAllListeners();
+        _btnOption.onClick.RemoveAllListeners();
+        _btnExit.onClick.RemoveAllListeners();
+    }
+
+    private void OnClickedStart()
+    {
+
+    }
+
+    private void OnClickedOption()
+    {
+
+    }
+
+    private void OnClickedExit()
+    {
+
+    }
+}
