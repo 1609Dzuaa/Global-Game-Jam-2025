d0cadf50f9308ccff4404e8fde8b7647f92156d1
KaneV <votrungkien05032002@gmail.com>
2025-01-18 21:03:13 +0700

    fix wrong names

--- Assets/Scripts/Panel/PanelTimer.cs
+++ Assets/Scripts/Panel/PanelTimer.cs
@@ -1,25 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PanelTimer : MonoBehaviour
 {
-    [SerializeField] Image _imageForce;
+    [SerializeField]
+    GameObject blueImage;
 
-    private void Awake()
+    [SerializeField]
+    TMPro.TextMeshProUGUI blueImageText;
+
+    [SerializeField]
+    GameObject redImage;
+
+    [SerializeField]
+    TMPro.TextMeshProUGUI redImageText;
+
+    [SerializeField]
+    float duration = 40f;
+
+    private void Start()
     {
-        EventsManager.Subcribe(EventID.OnSendSliderForce, HandleImage);
+        blueImage.SetActive(true);
+        redImage.SetActive(false);
+
+        HandleImage();
     }
 
-    private void OnDestroy()
+    private void HandleImage()
     {
-        EventsManager.Unsubcribe(EventID.OnSendSliderForce, HandleImage);
-    }
+        TextMeshProUGUI timerText = blueImageText;
+        float timeRemaining = duration;
 
-    private void HandleImage(object obj)
-    {
-        float value = (float)obj;
-        _imageForce.fillAmount = value;
+        float elapsedTime = 0f;
+
+        DOTween
+            .To(() => timeRemaining, x => timeRemaining = x, 0f, duration)
+            .OnUpdate(() =>
+            {
+                // Update elapsed time with deltaTime
+                elapsedTime += Time.deltaTime;
+                timeRemaining = Mathf.Max(0, duration - elapsedTime);
+
+                // Update the timer text
+                if (timeRemaining >= 60)
+                {
+                    timerText.text =
+                        Mathf.FloorToInt(timeRemaining / 60).ToString("00")
+                        + ":"
+                        + Mathf.CeilToInt(timeRemaining % 60).ToString("00");
+                }
+                else
+                {
+                    timerText.text = Mathf.CeilToInt(timeRemaining).ToString();
+                }
+
+                if (timeRemaining / duration < 0.3f)
+                {
+                    blueImage.SetActive(true);
+                    redImage.SetActive(false);
+
+                    timerText = redImageText;
+                }
+                else
+                {
+                    blueImage.SetActive(true);
+                    redImage.SetActive(false);
+                }
+            })
+            .OnComplete(() =>
+            {
+                EventsManager.Notify(EventID.OnTimerEnds, null);
+            });
     }
 }
